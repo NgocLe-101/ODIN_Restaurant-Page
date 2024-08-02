23d1b2d71871b409cc4f10d5fb898897927d3efa
NgocLe-101 <ngocle266tqt@gmail.com>
2024-08-02 16:34:30 +0700

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Restaurant Page',
+      template: './src/index.html',
+      inject: 'head',
+      scriptLoading: 'defer'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+};
